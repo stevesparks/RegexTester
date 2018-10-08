97251c5c6083badfc59b9df19c8a04ae837439cb
Steve Sparks <sparks@bignerdranch.com>
2018-10-07 20:40:58 -0400

    Initial Commit

---
+++ Regex Tester/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Regex Tester
+//
+//  Created by Steve Sparks on 10/7/18.
+//  Copyright © 2018 SOG. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Regex Tester/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Regex Tester
+//
+//  Created by Steve Sparks on 10/7/18.
+//  Copyright © 2018 SOG. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
